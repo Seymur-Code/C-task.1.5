87cd9f773e7b78b4e459baa2a2e9d04c6eb73ad6
seymu <seymu@214-14>
2021-10-29 09:43:57 +0400

    Add project files.

---
+++ C-task.1.5/Home.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_task._1._5
+{
+    class Home
+    {
+        /*  public string otaqSayi;
+          public string metrebeSayi;
+          public string unvan;
+          public string telefon;
+          public string seher;
+
+          public Home(string unvan, string telefon, string seher)
+          {
+              this.unvan = unvan;
+              this.telefon = telefon;
+              this.seher = seher;
+          }
+
+
+          public string melumat ()
+          {
+
+              return  ($" {seher}  {unvan}  {telefon}");
+
+          }*/
+
+        public string Citys;
+
+
+        public Home(string Citys)
+        {
+            this.Citys = Citys;
+        }
+
+        
+       
+
+    }
+}
---
+++ C-task.1.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace C_task._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Home home1 = new Home("Sulutepe", "+994073792939", "Baku,Azerbaijan" );
+
+             Console.WriteLine(home1.melumat());*/
+
+            /*Home citys1 = new Home("Tovuz");
+            Home citys2 = new Home("Gence");
+            Home citys3 = new Home("Baku");
+            Home citys4 = new Home("Qebele");
+
+
+           Home[] city1 = new Home[] {citys1, citys2, citys3, citys4};
+
+            
+            for (int i = 0; i < city1.Length; i++)
+            {
+                Console.WriteLine(city1[i].Citys);
+            }*/
+            
+         }
+    }
+}
